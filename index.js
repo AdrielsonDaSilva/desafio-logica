e3c9328d47ab91bd4f9b21d6a344b3793b4e7c0b
Adrielson da Silva <112909164+AdrielsonDaSilva@users.noreply.github.com>
2024-10-05 17:31:10 -0300

    Update index.js
    - Voltei a ler a questão e pedia um laço de repetição, então fiz apenas uma validação se o nome era diferente de vazio, e utilizei o break para sair do loop.

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 let nomeHeroi = "Super"
 let xp = 1500
 
-if (xp <= 1000) {
-    //ferro
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível **Ferro**`)
-}else if(xp > 1000 && xp <= 2000){
-    //bronze
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível **Bronze**`)
-}else if(xp > 2000 && xp <= 5000){
-    //ouro
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível **Ouro**`)
-}else if(xp > 5000 && xp <= 8000){
-    //platina
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível **Platina**`)
-}else if(xp > 8000 && xp <= 9000){
-    //ascendente
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível **Ascendente**`)
-}else if(xp > 9000 && xp <= 10000){
-    //imortal
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível **Imortal**`)
-}else{
-    //radiante
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível **Radiante**`)
+while (nomeHeroi != null) {
+    if (xp <= 1000) {
+        //ferro
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível **Ferro**`)
+        break
+    } else if (xp > 1000 && xp <= 2000) {
+        //bronze
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível **Bronze**`)
+        break
+    } else if (xp > 2000 && xp <= 5000) {
+        //ouro
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível **Ouro**`)
+        break
+    } else if (xp > 5000 && xp <= 8000) {
+        //platina
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível **Platina**`)
+        break
+    } else if (xp > 8000 && xp <= 9000) {
+        //ascendente
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível **Ascendente**`)
+        break
+    } else if (xp > 9000 && xp <= 10000) {
+        //imortal
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível **Imortal**`)
+        break
+    } else {
+        //radiante
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível **Radiante**`)
+        break
+    }
 }
